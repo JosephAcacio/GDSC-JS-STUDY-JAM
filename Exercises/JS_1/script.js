c8fb3b0e0575e307fad96e911ce8ab236188ecda
Joseph Acacio <josephcacacio@iskolarngbayan.pup.edu.ph>
2025-02-27 22:46:07 +0800

    JS Variables Types

---
+++ Exercises/JS_1/script.js
@@ -0,0 +1,34 @@
+let name = "Tyler Hawk"; // Define a string here, e.g., "Juan Cruz"
+let age = 19; // Define a number here, e.g., 30
+let isStudent = false; // Define a boolean here, e.g., true or false
+let hobbies = "Photography"; // Define an array here, e.g., ["reading", "gaming", "coding"]
+
+// Checking types and values
+console.log(typeof name); // Output: should be "string"  (until a value is assigned)
+console.log(typeof age); // Output: should be "number" (until a value is assigned)
+console.log(typeof isStudent); // Output:  should be "boolean" (until a value is assigned)
+console.log(typeof hobbies); // Output: should be "object" (until a value is assigned)
+
+// Using const
+const pi = 3.14159; // Define a number here, e.g., 3.14159
+const birthYear = 2005; // Define a number here, e.g., 1990
+const isEmployed = true; // Define a boolean here, e.g., true or false
+const favoriteBooks = "Hunger Games"; // Define an array here, e.g., ["1984", "Brave New World", "Fahrenheit 451"]
+
+// Checking types and values
+console.log(typeof pi); // Output: should be "number" (until a value is assigned)
+console.log(typeof birthYear); // Output: should be "number" (until a value is assigned)
+console.log(typeof isEmployed); // Output: should be "boolean" (until a value is assigned)
+console.log(typeof favoriteBooks); // Output: should be "object" (until a value is assigned)
+
+// Using var
+var city = "California"; // Define a string here, e.g., "New York"
+var temperature = 33.6; // Define a number here, e.g., 22.5
+var hasPets = true; // Define a boolean here, e.g., true or false
+var colors = ["Red", "Blue", "Green"]; // Define an array here, e.g., ["red", "green", "blue"]
+
+// Checking types and values
+console.log(typeof city); // Output: should be "string" (until a value is assigned)
+console.log(typeof temperature);  // Output: should be "number" (until a value is assigned)
+console.log(typeof hasPets); // Output: should be "boolean" (until a value is assigned)
+console.log(typeof colors); // Output: should be "object" (until a value is assigned)
